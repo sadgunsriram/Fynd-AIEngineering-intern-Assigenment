2fe9fd477ef3db540c6edf4a60a42e6a0878cb9f
sadgunsriram <121039739+sadgunsriram@users.noreply.github.com>
2025-12-07 23:39:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = "super-secret-key"
-    # ?SQLALCHEMY_DATABASE_URI = "sqlite:///instance/app.db"
-    
-    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/app.db"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
